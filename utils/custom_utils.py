84ac686612898607c278157c5a54feb7bb208765
phuong83033-pixel <phuong83033@gmail.com>
2026-06-22 16:00:47 +0000

    Create gold transform

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -1,13 +1,18 @@
 from typing import List
 from pyspark.sql import DataFrame
 from pyspark.sql.window import Window
+from pyspark.sql.functions import *
 class transformation:
     
-    def dedup(self,df,dedub_cols:List,cdc:str):
+    def dedup(self,df,dedup_cols:List,cdc:str):
          
         df = df.withColumn("dedupKey", concat(*dedup_cols))
-        df.withColumn("dedupCounts", row_number().over(Window.partitonBy("dedupKey").orderBy(cdc)))
+        df = df.withColumn("dedupCounts",row_number().over(Window.partitionBy("dedupKey").orderBy(desc(cdc))))
         df = df.filter(col('dedupCounts') == 1)
-        df = df.drop("dedupKey", "dedupCounts")
+        df = df.drop("dedupKefile y", "dedupCounts")
 
+        return df
+
+    def process_timestamp(self,df):
+        df = df.withColumn("process_timestamp",current_timestamp())
         return df
